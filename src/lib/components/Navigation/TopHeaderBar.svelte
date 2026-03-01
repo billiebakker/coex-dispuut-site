718daabf6901fc970e02e13f2d00293338632d2f
Billie Bakker <billie@zooma.nl>
2026-03-01 14:50:18 +0100

    add back button to topbar

--- src/lib/components/Navigation/TopHeaderBar.svelte
+++ src/lib/components/Navigation/TopHeaderBar.svelte
@@ -1,25 +1,32 @@
 <script>
 	import { page } from '$app/state';
 
+	const topRoutes = ['/home', '/activiteiten', '/account'];
+
+	const showBackButton = $derived(!topRoutes.includes(page.url.pathname));
+
+	function handleBack() {
+		history.back();
+	}
+
 	let currentTitle = $derived.by(() => {
-		switch (page.url.pathname) {
-			case '/home':
-				return 'Home';
-			case '/activiteiten':
-				return 'Activiteiten';
-			case '/account':
-				return 'Account';
-			default:
-				return 'Ribbook';
-		}
+		if (page.url.pathname === '/home') return 'Home';
+		if (page.url.pathname.startsWith('/activiteiten')) return 'Activiteiten';
+		if (page.url.pathname === '/account') return 'Account';
+		return 'Ribbook';
 	});
 </script>
 
 <header class="bg-ribbook-red relative flex h-16 w-full shrink-0 items-center justify-center">
 	<div class="text-ribbook-yellow font-red-october text-[30px]">{currentTitle}</div>
-	<!-- <a href="" class="absolute top-0 left-3.5 flex h-full cursor-pointer items-center justify-center"> -->
-	<!-- v-if="!['home', 'events', 'account'].includes($route.name)"
-      @click="$router.back()" -->
-	<!-- <span class="icon icon-40 icon-yellow">Arrow_Back</span> -->
-	<!-- </a> -->
+	{#if showBackButton}
+		<button
+			type="button"
+			onclick={handleBack}
+			class="absolute top-0 left-3.5 flex h-full cursor-pointer items-center justify-center"
+			aria-label="Ga terug"
+		>
+			<span class="icon icon-40 icon-yellow">Arrow_Back</span>
+		</button>
+	{/if}
 </header>
